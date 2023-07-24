e88dd626ad333e92b30372f9506881c08dfdce6e
Eren-1Yeager <95918351+Eren-1Yeager@users.noreply.github.com>
2023-07-24 18:25:29 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x54fbc62EDff786981114618b7a0DfC31fCfbeBB5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
